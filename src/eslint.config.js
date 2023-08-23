90ce001f0628b1a112ee6092df83de266eb78e01
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-23 00:31:32 -0600

    Update prettier

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,15 +1,18 @@
 import configPrettier from "eslint-config-prettier";
 import pluginPrettier from "eslint-plugin-prettier";
-import baseConfig from "./base.config.js"
-import reactConfig from "./react.config.js"
-import testConfig from "./test.config.js"
+import baseConfig from "./base.config.js";
+import reactConfig from "./react.config.js";
+import testConfig from "./test.config.js";
 
 export default [
-	...baseConfig,
-	...reactConfig,
-	...testConfig,
-	{
-		...configPrettier,
-		plugins: { prettier: pluginPrettier }
-	},
-]
+  ...baseConfig,
+  ...reactConfig,
+  ...testConfig,
+  configPrettier,
+  {
+    plugins: { prettier: pluginPrettier },
+    rules: {
+      ...pluginPrettier.configs.recommended.rules,
+    },
+  },
+];
