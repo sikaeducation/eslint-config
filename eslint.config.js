eb2e99b9cf380c1358e56d65d894759ef07ae254
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-22 22:14:40 -0600

    WIP

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,6 @@ export default [
 	...testConfig,
 	{
 		...configPrettier,
-		plugins: { pluginPrettier }
+		plugins: { prettier: pluginPrettier }
 	},
 ]
--- test.config.js
+++ test.config.js
@@ -1,7 +1,6 @@
 import pluginJest from "eslint-plugin-jest"
 import pluginStorybook from "eslint-plugin-storybook"
-import pluginImport from "eslint-plugin-import"
-import * as mdx from "eslint-plugin-mdx"
+// import * as mdx from "eslint-plugin-mdx"
 
 export default [{
 	files: [
@@ -9,6 +8,7 @@ export default [{
 		"**/features/**/*.{ts,tsx}",
 		"**/setupTests.{js,ts}",
 		"**/reportWebVitals.{js,ts}",
+		"**/*.{mdx}",
 	],
 	rules: {
 		"import/no-extraneous-dependencies": [
@@ -25,7 +25,6 @@ export default [{
 	},
 	plugins: {
 		jest: pluginJest,
-		import: pluginImport,
 	},
 },
 {
@@ -42,12 +41,12 @@ export default [{
 		"testing-library/prefer-screen-queries": "off",
 	},
 },
-{
-	...mdx.flat,
-	processor: mdx.createRemarkProcessor({
-		// lintCodeBlocks: true,
-	}),
-},
+// {
+// 	...mdx.flat,
+// 	processor: mdx.createRemarkProcessor({
+// 		// lintCodeBlocks: true,
+// 	}),
+// },
 // {
 // 	...mdx.flatCodeBlocks,
 // 	rules: {
@@ -58,15 +57,7 @@ export default [{
 	files: ["**/*.stories.*", "**/*.mdx"],
 	rules: {
 		...pluginStorybook.configs.recommended.rules,
-		"import/no-anonymous-default-export": "off",
-		"react/function-component-definition": "off",
 		"react-hooks/rules-of-hooks": "off",
-		"react/jsx-props-no-spreading": "off",
-		"import/no-extraneous-dependencies": "off",
-		"@typescript-eslint/no-empty-function": "off",
-		"no-console": "off",
-		"react/jsx-filename-extension": "off",
-		"react/destructuring-assignment": "off",
 	},
 	plugins: {
 		storybook: pluginStorybook,
