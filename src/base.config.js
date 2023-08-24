c24286857a3f0df6861b91c8ac93555449d14d92
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-23 19:56:59 -0600

    Fix import

--- src/base.config.js
+++ src/base.config.js
@@ -1,54 +1,54 @@
 import pluginPrettier from "eslint-plugin-prettier";
 import parserTS from "@typescript-eslint/parser";
 
-import compat from "./compat";
+import compat from "./compat.js";
 
 export default [
-  {
-    files: ["**/*.{js}"],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-      },
-    },
-  },
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        ecmaVersion: "latest",
-        project: true,
-      },
-    },
-  },
-  ...compat.extends("airbnb", "plugin:@typescript-eslint/recommended"),
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    linterOptions: {
-      reportUnusedDisableDirectives: false,
-    },
-    languageOptions: {
-      parser: parserTS,
-    },
-    settings: {
-      "import/parsers": {
-        espree: [".js", ".cjs", ".mjs", ".jsx"],
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
-      "import/resolver": {
-        typescript: {
-          extensions: [".ts", ".tsx"],
-        },
-        node: {
-          extensions: [".js", ".jsx"],
-        },
-      },
-    },
-    plugins: {
-      prettier: pluginPrettier,
-    },
-  },
+	{
+		files: ["**/*.{js}"],
+		languageOptions: {
+			parserOptions: {
+				ecmaVersion: "latest",
+			},
+		},
+	},
+	{
+		files: ["**/*.{ts,tsx}"],
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+				ecmaVersion: "latest",
+				project: true,
+			},
+		},
+	},
+	...compat.extends("airbnb", "plugin:@typescript-eslint/recommended"),
+	{
+		files: ["**/*.{js,jsx,ts,tsx}"],
+		linterOptions: {
+			reportUnusedDisableDirectives: false,
+		},
+		languageOptions: {
+			parser: parserTS,
+		},
+		settings: {
+			"import/parsers": {
+				espree: [".js", ".cjs", ".mjs", ".jsx"],
+				"@typescript-eslint/parser": [".ts", ".tsx"],
+			},
+			"import/resolver": {
+				typescript: {
+					extensions: [".ts", ".tsx"],
+				},
+				node: {
+					extensions: [".js", ".jsx"],
+				},
+			},
+		},
+		plugins: {
+			prettier: pluginPrettier,
+		},
+	},
 ];
--- src/test.config.js
+++ src/test.config.js
@@ -1,87 +1,87 @@
 import pluginJest from "eslint-plugin-jest";
 import pluginStorybook from "eslint-plugin-storybook";
-import compat from "./compat";
+import compat from "./compat.js";
 
 export default [
-  {
-    files: [
-      "**/*.{test,spec}.{ts,tsx,js,jsx}",
-      "**/features/**/*.{ts,tsx}",
-      "**/setupTests.{js,ts}",
-      "**/reportWebVitals.{js,ts}",
-      "**/*.{mdx}",
-    ],
-    rules: {
-      "import/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: [
-            "**/*.{test,spec}.{ts,tsx,js,jsx}",
-            "**/features/**/*.{ts,tsx}",
-            "**/setupTests.{js,ts}",
-            "**/reportWebVitals.{js,ts}",
-          ],
-        },
-      ],
-    },
-    plugins: {
-      jest: pluginJest,
-    },
-  },
-  {
-    files: ["**/test-runner.js"],
-    rules: {
-      "@typescript-eslint/no-var-requires": "off",
-    },
-  },
-  {
-    files: ["**/features/**/*.ts"],
-    rules: {
-      "func-names": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-      "testing-library/prefer-screen-queries": "off",
-    },
-  },
-  ...compat.extends("plugin:mdx/recommend"),
-  {
-    files: ["**/*.{md,mdx}"],
-    settings: {
-      "mdx/code-block": true,
-      "import/parsers": {
-        espree: [".js", ".cjs", ".mjs", ".jsx"],
-        "@typescript-eslint/parser": [".ts", ".tsx"],
-      },
-      "import/resolver": {
-        typescript: {
-          extensions: [".ts", ".tsx"],
-        },
-        node: {
-          extensions: [".js", ".jsx"],
-        },
-      },
-    },
-  },
-  {
-    files: ["**/*.stories.*", "**/*.mdx"],
-    rules: {
-      ...pluginStorybook.configs.recommended.rules,
-      "react-hooks/rules-of-hooks": "off",
-      "react/jsx-props-no-spreading": "off",
-      "react/jsx-filename-extension": [
-        "error",
-        {
-          extensions: [".mdx", ".jsx", ".tsx"],
-        },
-      ],
-      "import/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: ["**/*.stories.*", "**/*.mdx"],
-        },
-      ],
-    },
-    plugins: {
-      storybook: pluginStorybook,
-    },
-  },
+	{
+		files: [
+			"**/*.{test,spec}.{ts,tsx,js,jsx}",
+			"**/features/**/*.{ts,tsx}",
+			"**/setupTests.{js,ts}",
+			"**/reportWebVitals.{js,ts}",
+			"**/*.{mdx}",
+		],
+		rules: {
+			"import/no-extraneous-dependencies": [
+				"error",
+				{
+					devDependencies: [
+						"**/*.{test,spec}.{ts,tsx,js,jsx}",
+						"**/features/**/*.{ts,tsx}",
+						"**/setupTests.{js,ts}",
+						"**/reportWebVitals.{js,ts}",
+					],
+				},
+			],
+		},
+		plugins: {
+			jest: pluginJest,
+		},
+	},
+	{
+		files: ["**/test-runner.js"],
+		rules: {
+			"@typescript-eslint/no-var-requires": "off",
+		},
+	},
+	{
+		files: ["**/features/**/*.ts"],
+		rules: {
+			"func-names": "off",
+			"@typescript-eslint/ban-ts-comment": "off",
+			"testing-library/prefer-screen-queries": "off",
+		},
+	},
+	...compat.extends("plugin:mdx/recommend"),
+	{
+		files: ["**/*.{md,mdx}"],
+		settings: {
+			"mdx/code-block": true,
+			"import/parsers": {
+				espree: [".js", ".cjs", ".mjs", ".jsx"],
+				"@typescript-eslint/parser": [".ts", ".tsx"],
+			},
+			"import/resolver": {
+				typescript: {
+					extensions: [".ts", ".tsx"],
+				},
+				node: {
+					extensions: [".js", ".jsx"],
+				},
+			},
+		},
+	},
+	{
+		files: ["**/*.stories.*", "**/*.mdx"],
+		rules: {
+			...pluginStorybook.configs.recommended.rules,
+			"react-hooks/rules-of-hooks": "off",
+			"react/jsx-props-no-spreading": "off",
+			"react/jsx-filename-extension": [
+				"error",
+				{
+					extensions: [".mdx", ".jsx", ".tsx"],
+				},
+			],
+			"import/no-extraneous-dependencies": [
+				"error",
+				{
+					devDependencies: ["**/*.stories.*", "**/*.mdx"],
+				},
+			],
+		},
+		plugins: {
+			storybook: pluginStorybook,
+		},
+	},
 ];
