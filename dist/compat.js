25752052683f448a4b2fefef352b71b4639ce7f3
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-24 18:40:02 -0600

    Add babel

--- dist/compat.js
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-const nodeModulesPath = path.dirname("/node_modules");
-export default new FlatCompat({
-    baseDirectory: nodeModulesPath,
-    resolvePluginsRelativeTo: nodeModulesPath,
-});
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,48 @@
-export { default as javascript } from "./javascript.config.js";
-export { default as featureTests } from "./feature-tests.config.js";
-export { default as react } from "./react.config.js";
-export { default as jest } from "./jest.config.js";
-export { default as storybook } from "./storybook.config.js";
-export { default as formatting } from "./formatting.config.js";
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "featureTests", {
+  enumerable: true,
+  get: function get() {
+    return _featureTestsConfig["default"];
+  }
+});
+Object.defineProperty(exports, "formatting", {
+  enumerable: true,
+  get: function get() {
+    return _formattingConfig["default"];
+  }
+});
+Object.defineProperty(exports, "javascript", {
+  enumerable: true,
+  get: function get() {
+    return _javascriptConfig["default"];
+  }
+});
+Object.defineProperty(exports, "jest", {
+  enumerable: true,
+  get: function get() {
+    return _jestConfig["default"];
+  }
+});
+Object.defineProperty(exports, "react", {
+  enumerable: true,
+  get: function get() {
+    return _reactConfig["default"];
+  }
+});
+Object.defineProperty(exports, "storybook", {
+  enumerable: true,
+  get: function get() {
+    return _storybookConfig["default"];
+  }
+});
+var _javascriptConfig = _interopRequireDefault(require("./javascript.config.js"));
+var _featureTestsConfig = _interopRequireDefault(require("./feature-tests.config.js"));
+var _reactConfig = _interopRequireDefault(require("./react.config.js"));
+var _jestConfig = _interopRequireDefault(require("./jest.config.js"));
+var _storybookConfig = _interopRequireDefault(require("./storybook.config.js"));
+var _formattingConfig = _interopRequireDefault(require("./formatting.config.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
