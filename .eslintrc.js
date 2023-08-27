d1f967cfa3d3f1e228cf6d78d75e08bc6c7516a6
Kyle Coberly <kyle.coberly@gmail.com>
2023-08-26 23:45:12 -0600

    Fix imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
 	overrides: [
 		require("@sikaeducation/eslint-config-ts-react/js"),
 		require("@sikaeducation/eslint-config-ts-react/formatting"),
+		{
+			files: ["**/*.js"],
+			rules: {
+				"import/no-unresolved": "off",
+			},
+			plugins: ["import"],
+		},
 	],
 };
